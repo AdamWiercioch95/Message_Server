4f52eedba2e6af9275495dea8b8d8e785e9c4c05
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-04-30 15:35:04 +0200

    modified crypto.py models.py

--- models.py
+++ models.py
@@ -1,78 +1,77 @@
-from psycopg2 import connect
+from crypto import hash_password
 
 
 class User:
-    def __init__(self, username="", password=""):
+    def __init__(self, username="", password="", salt=""):
         self._user_id = -1
         self.username = username
-        self._password = password
+        self._hashed_password = hash_password(password, salt)
 
     @property
     def user_id(self):
         return self._user_id
 
     @property
-    def password(self):
-        return self._password
+    def hashed_password(self):
+        return self._hashed_password
 
-    def set_password(self, password):
-        self._password = password
+    def set_password(self, password, salt=""):
+        self.hashed_password = hash_password(password, salt)
+
+    @hashed_password.setter
+    def hashed_password(self, password):
+        self.set_password(password)
 
     def save_to_db(self, crs):
         if self._user_id == -1:
-            query = f"""
-                INSERT INTO users (username, password) VALUES (
-                '{self.username}', '{self.password}'
-                )
+            query = """
+                INSERT INTO users (username, password) VALUES (%s, %s)
                 RETURNING id
             """
-            crs.execute(query)
+            crs.execute(query, (self.username, self.hashed_password))
             self._user_id = crs.fetchone()[0]
+            return True
         else:
             query = f"""
-                UPDATE users SET 
-                username = '{self.username}',
-                password = '{self.password}'
-                WHERE id = {self._user_id};
+                UPDATE users 
+                SET username = %s, password = %s
+                WHERE id = %s;
             """
-            crs.execute(query)
+            crs.execute(query, (self.username, self.hashed_password, self._user_id))
+            return True
 
     @staticmethod
     def load_user_by_username(crs, username):
         query = f"""
-            SELECT id, username, password FROM users WHERE username = '{username}'
+            SELECT id, username, password FROM users WHERE username = %s
         """
-        crs.execute(query)
+        crs.execute(query, (username,))
         data = crs.fetchone()
         if data:
             id_, username, password = data
             loaded_user = User(username)
             loaded_user._user_id = id_
-            loaded_user._password = password
+            loaded_user._hashed_password = password
             return loaded_user
-        else:
-            return None
 
     @staticmethod
     def load_user_by_id(crs, user_id):
         query = f"""
-            SELECT id, username, password FROM users WHERE id={user_id}
+            SELECT id, username, password FROM users WHERE id=%s
             """
-        crs.execute(query)
+        crs.execute(query, (user_id,))
         data = crs.fetchone()
         if data:
             id_, username, password = data
             loaded_user = User(username)
             loaded_user._user_id = id_
-            loaded_user._password = password
+            loaded_user._hashed_password = password
             return loaded_user
-        else:
-            return None
 
     @staticmethod
     def load_all_users(crs):
         query = f"""
-            SELECT * FROM users
+            SELECT id, username, password FROM users
         """
         users = []
         crs.execute(query)
@@ -80,16 +79,16 @@ class User:
             id_, username, password = row
             loaded_user = User()
             loaded_user._user_id = id_
-            loaded_user._password = password
+            loaded_user._hashed_password = password
             loaded_user.username = username
             users.append(loaded_user)
         return users
 
     def delete(self, crs):
         query = f"""
-            DELETE FROM users WHERE id={self._user_id}
+            DELETE FROM users WHERE id=%s
         """
-        crs.execute(query)
+        crs.execute(query, (self._user_id,))
         self._user_id = -1
         return True
 
@@ -113,36 +112,38 @@ class Message:
     def save_to_db(self, crs):
         if self._id == -1:
             query = f"""
-                INSERT INTO messages (from_id, to_user_id, text) VALUES (
-                    {self.from_user_id}, {self.to_user_id}, '{self.content}'
-                );
+                INSERT INTO messages (from_id, to_user_id, text) VALUES (%s, %s, %s);
                 RETURNING id, creation_date;
             """
-            crs.execute(query)
+            crs.execute(query, (self.from_user_id, self.to_user_id, self.text))
             self._id, self_creation_date = crs.fetchone()
+            return True
         # poprawić create_db.py -> nazwy column w tabeli
         else:
             query = f"""
                 UPDATE messages SET 
-                    from_id={self.from_user_id}, 
-                    to_user_id={self.to_user_id}, 
-                    text='{self.text}'
-                    WHERE id = {self._id};
+                    from_id=%s, 
+                    to_user_id=%s, 
+                    text=%s
+                    WHERE id=%s;
             """
-            crs.execute(query)
+            crs.execute(query, (self.from_user_id, self.to_user_id, self.text, self._id))
+            return True
 
     @staticmethod
     def load_all_messages(crs, id_=None):
         if id_ is None:
             query = f"""
-                SELECT * FROM messages
+                SELECT id, from_id, to_id, text, creation_date FROM messages
             """
+            crs.execute(query)
         else:
             query = f"""
-                SELECT * FROM messages WHERE id={id_};
+                SELECT id, from_id, to_id, text, creation_date FROM messages WHERE id=%s;
             """
+            crs.execute(query, (id_,))
+
         messages = []
-        crs.execute(query)
         for row in crs.fetchall():
             id_, from_id, to_user_id, text, creation_date = row
             loaded_message = Message(from_id, to_user_id, text)
@@ -150,35 +151,3 @@ class Message:
             loaded_message._creation_date = creation_date
             messages.append(loaded_message)
         return messages
-
-
-# settings = {
-#     'host': 'localhost',
-#     'database': 'message_server_db',
-#     'user': 'postgres',
-#     'password': 'coderslab',
-#     'port': 5432
-# }
-# connection = connect(**settings)
-# cursor = connection.cursor()
-#
-# result1 = User.load_user_by_id(cursor, 4)
-# print(result1._user_id, result1._password, result1.username)
-#
-# result2 = User.load_all_users(cursor)
-# print("wyświetlam wszystkich")
-# for user in result2:
-#     print(user.user_id, user.username, user.password)
-#
-# print("Usuwam")
-# result1.delete(cursor)
-# connection.commit()
-#
-# result2 = User.load_all_users(cursor)
-# print("wyświetlam wszystkich")
-# for user in result2:
-#     print(user.user_id, user.username, user.password)
-#
-# connection.commit()
-# cursor.close()
-# connection.close()
